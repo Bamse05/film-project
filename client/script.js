fe640bac720eae7fb47b00dffa434217b4e41843
Senator Armstrong <loke.lindstrom@outlook.com>
2026-02-24 15:50:48 +0100

    Gets data as object not array with one object inside

--- client/script.js
+++ client/script.js
@@ -19,7 +19,7 @@ async function reqMovieData(id) {
 
     if (response.ok) {
         return response.json().then((jsonBody) => {
-            return jsonBody;
+            return jsonBody[0];
         })
     } else {
         console.log("Could not get data. Error code: " + response.status);
@@ -38,7 +38,7 @@ async function reqActorData(id) {
 
     if (response.ok) {
         return response.json().then((jsonBody) => {
-            return jsonBody;
+            return jsonBody[0];
         })
     } else {
         console.log("Could not get data. Error code: " + response.status);
@@ -66,3 +66,19 @@ async function reqIdList(dbCollection) {
 }
 
 // =============================
+
+
+
+async function deletethis() {
+    data1 = await reqMovieData("tt0021739");
+    data2 = await reqActorData("nm0000194");
+    data3 = await reqIdList(dbCollectionImdb);
+    data4 = await reqIdList(dbCollectionActorinfo);
+
+    console.log(data1);
+    console.log(data2);
+    console.log(data3);
+    console.log(data4);
+}
+
+deletethis();
